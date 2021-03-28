574107f68dafb970d925b3136904a439ecccef61
rwaeng <80890092+rwaeng@users.noreply.github.com>
2021-03-29 00:25:40 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+import './App.css';
+
+
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World wide web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperText markup Language.'},
+        {id:2, title:'CSS', desc:'CSS is for design.'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interaction'},
+      ]
+    }
+  }
+  render() {
+    return(
+      <div className="App">
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}> 
+        </Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText markup Language."></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
